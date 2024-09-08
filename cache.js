f751384c5be90c15f8b39d12fd95152bc1406454
NetBy <netbyofficial@gmail.com>
2024-09-08 15:30:04 +0200

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,5 +1,5 @@
 //(function() {
-    var version = '1.0.9';  // Change this version number whenever you make updates
+    var version = '1.1.0';  // Change this version number whenever you make updates
 
     // List of files to update
     var filesToCacheBust = [
